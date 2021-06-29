bd20f6f53ee16ed13191fccacdada11959e5d412
Philip Ramirez <pramirez2@apple.com>
2021-06-29 10:01:33 -0700

    finished game simulation

--- README.md
+++ README.md
@@ -24,13 +24,28 @@ run calculate_metrics.py
 
 run cleanup_data.py
 
+## STEP 6
+
+run estimate_scoring_rates.py
+run get_scoring_distrib.py
+run simulate_goals.py
+  runs simulate_goals_solo.py
+
 ## TO DO
 * calculate suspense
 * event takes place after end
 * first half wrong in lots of games. random active flags are messing it up
-  remove active flags before game start 
+  remove active flags before game start
 * redo sentiment model
 
-## Questions
-* should shock, surprise, suspense be None before the match starts?
-* how to hand nuetral events
+# suspense
+* join in red card events
+* scorelines are recorded of each of the simulation
+* iterate through every minute
+  * at each minute given the current score
+    each minute uses a little bit of real time data (current score) and the rest is simulated
+    given current score, in the given minute... simulate the rest of the match 100,000 times
+  * also record whether there's a goal in the next minute
+  * will be a subset in the simulations where this is the case
+
+* use
--- src/simulate_goals.py
+++ src/simulate_goals.py
@@ -4,35 +4,68 @@ step 3 of calculating suspense
 
 import os
 import sys
-import time
 import json
-import subprocess
+import pandas as pd
 from queue import Queue
 from threading import Thread
+from operator import itemgetter
+from scipy.stats import bernoulli
+
+TRIALS = 100000
 
 DATA_DIR = '../data/'
+OUT_DIR = os.path.join(DATA_DIR, 'aggregated')
+SIM_DIR = os.path.join(DATA_DIR, 'simulations')
+DATA_DF = pd.read_csv(os.path.join(OUT_DIR, 'season_2013_agg_final_0504.csv'))
+DISTRIBUTION = pd.read_csv(os.path.join(DATA_DIR, 'scoring_distribution.csv'), index_col=0)
+
+# get teams per match
+team_df = DATA_DF[['Event ID', 'selection_home', 'selection_away']].drop_duplicates()
+team_df['Event ID'] = team_df['Event ID'].astype(int).astype(str)
+teams = team_df.set_index('Event ID').T.to_dict()
+
+# get scoring rates
 with open(os.path.join(DATA_DIR, 'scoring_rates.json'), 'r') as json_file:
     scoring_rates = json.load(json_file)
 
+def simulate(match_id):
+    home_team = teams[match_id]['selection_home']
+    away_team = teams[match_id]['selection_away']
+    scoring_rate_home = scoring_rates[match_id][home_team]
+    scoring_rate_away = scoring_rates[match_id][away_team]
+    match_distrib = DISTRIBUTION.copy()
+    match_distrib['expctd_goals_home'] = match_distrib['density'] * scoring_rate_home
+    match_distrib['expctd_goals_away'] = match_distrib['density'] * scoring_rate_away
+    home = match_distrib['expctd_goals_home'].apply(lambda x:bernoulli.rvs(x, size=TRIALS)).transform({f'{i}': itemgetter(i) for i in range(TRIALS)})
+    away = match_distrib['expctd_goals_away'].apply(lambda x:bernoulli.rvs(x, size=TRIALS)).transform({f'{i}': itemgetter(i) for i in range(TRIALS)})
+    home.loc['score'] = home.sum()
+    away.loc['score'] = away.sum()
+    home_file = os.path.join(SIM_DIR, f'{match_id}_home.csv')
+    away_file = os.path.join(SIM_DIR, f'{match_id}_away.csv')
+    home.to_csv(home_file)
+    away.to_csv(away_file)
+    return
+
+# multithread simulations
+print('queueing matches')
 q = Queue()
 for match_id in scoring_rates:
+    if os.path.isfile(os.path.join(SIM_DIR, f'{match_id}_home.csv')):
+        continue
     q.put(match_id)
 q.put(None)
 
-def sub_stuff(match):
-    print(f'runnning thread for match {match}')
-    terminalCommand = "python simulate_goals_solo.py -m " + match
-    subprocess.run(terminalCommand, shell=True)
-
 def worker():
     while True:
-        match = q.get()
-        if match is None:
+        match_id = q.get()
+        print(f'performing simulations for match {match_id}')
+        if match_id is None:
           return
 
-        sub_stuff(match)
+        simulate(match_id)
 
-num_workers = 10
+print('done queing, starting workers...')
+num_workers = 5
 threads = [Thread(target=worker) for _i in range(num_workers)]
 for thread in threads:
     thread.start()
